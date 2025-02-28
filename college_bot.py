5c8b6bc38a05150294fffebaabd1b81e0fabc6bb
Devi Saranya <149192257+SaranyaYanamadala@users.noreply.github.com>
2025-02-28 14:47:36 +0530

    Update college_bot.py

--- college_bot.py
+++ college_bot.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title = "SVECW College Chatbot", layout = "centered")
 if "messages" not in st.session_state :
         st.session_state.messages = []
 
-csv_url = "svecw_details.csv"
+csv_url = "svcew_details.csv"
 
 ## If Exception in reading the file 
 try :
